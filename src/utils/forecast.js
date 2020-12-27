46c66a9ee1bec2bd7b5b9a0037d5e3f314329e15
Serginni <serginnimarten@gmail.com>
2020-12-27 22:44:32 +0100

    Added humidity to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -4,11 +4,11 @@ const forecast = (latitude, longitude, callback) => {
 const url ='http://api.weatherstack.com/current?access_key=f34782415596a919b5cd186041e3c1c0&query='+ latitude + ',' + longitude + '&units=m'
 request({ url, json: true }, (error, { body }) => {
         if (error) {
-        callback('Unable to connect to location services!', undefined)
+            callback('Unable to connect to location services!', undefined)
         } else if (body.error) {
-        callback('Unable to find location! Try a different search.', undefined)
+            callback('Unable to find location! Try a different search.', undefined)
         } else  {
-        callback(undefined, body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degrees celsius out. It feels like " + body.current.feelslike + " degrees out.")
+            callback(undefined, body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degrees celsius out. It feels like " + body.current.feelslike + " degrees out. The humidity at the moment is " + body.current.humidity + "%.")
         }
     })
 }
